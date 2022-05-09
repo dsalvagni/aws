e2cf048ff81804917967cf12253a07a51b7d86ef
Daniel Salvagni <daniel.salvagni@gmail.com>
2022-05-09 11:08:27 +0100

    added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "aws": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "awstest": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
